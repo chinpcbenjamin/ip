6e7f80742ec8ea2ba31bb1ebba6b534e728df294
chinpcbenjamin <chinpcbenjamin@gmail.com>
2025-01-24 00:09:57 +0800

    Added ability to store text enter by user and display back to user when requested

--- src/main/java/BenjaminBot.java
+++ src/main/java/BenjaminBot.java
@@ -1,16 +1,27 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class BenjaminBot {
     public static void main(String[] args) {
+        ArrayList<String> strArr = new ArrayList<>(100);
         System.out.println("___________________________________________________________________");
         System.out.println("Hello! I'm BenjaminBot");
         System.out.println("What can I do for you?");
         System.out.println("___________________________________________________________________");
         Scanner sc = new Scanner(System.in);
-        String s = "";
+        String s = sc.nextLine();
+        System.out.println("___________________________________________________________________");
         while (!s.equals("bye")) {
-            s = sc.next();
-            System.out.println(s);
+            if (s.equals("list")) {
+                for (int i = 0; i < strArr.size(); i++) {
+                    System.out.println(i + 1 + ". " + strArr.get(i));
+                }
+            } else {
+                strArr.add(s);
+                System.out.println("added: " + s);
+            }
+            System.out.println("___________________________________________________________________");
+            s = sc.nextLine();
             System.out.println("___________________________________________________________________");
         }
         System.out.println("Bye. Hope to see you again soon!");
