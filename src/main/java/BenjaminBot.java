c5345ec19a712e0b8433394311ae40ba6f3fea76
chinpcbenjamin <chinpcbenjamin@gmail.com>
2025-01-23 23:16:09 +0800

    Implemented echo feature for BenjaminBot

--- src/main/java/BenjaminBot.java
+++ src/main/java/BenjaminBot.java
@@ -1,9 +1,18 @@
+import java.util.Scanner;
+
 public class BenjaminBot {
     public static void main(String[] args) {
         System.out.println("___________________________________________________________________");
         System.out.println("Hello! I'm BenjaminBot");
         System.out.println("What can I do for you?");
         System.out.println("___________________________________________________________________");
+        Scanner sc = new Scanner(System.in);
+        String s = "";
+        while (!s.equals("bye")) {
+            s = sc.next();
+            System.out.println(s);
+            System.out.println("___________________________________________________________________");
+        }
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println("___________________________________________________________________");
     }
