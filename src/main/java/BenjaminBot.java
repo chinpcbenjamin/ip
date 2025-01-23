09beac8ad305dc98e2836e8645b36136d1142804
chinpcbenjamin <chinpcbenjamin@gmail.com>
2025-01-23 23:04:47 +0800

    Renamed Duke.java to BenjaminBot.java, and added Greet and Exit

---
+++ src/main/java/BenjaminBot.java
@@ -0,0 +1,10 @@
+public class BenjaminBot {
+    public static void main(String[] args) {
+        System.out.println("___________________________________________________________________");
+        System.out.println("Hello! I'm BenjaminBot");
+        System.out.println("What can I do for you?");
+        System.out.println("___________________________________________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("___________________________________________________________________");
+    }
+}
